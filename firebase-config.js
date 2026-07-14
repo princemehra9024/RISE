b067a627e88a86c83277723551cbd2614e0adee3
Prince Mehra <princemehra9024@gamil.com>
2026-07-15 01:01:30 +0530

    Please provide the code or description of the changes you have made so I can generate the commit message for you.

--- firebase-config.js
+++ firebase-config.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 // TODO: Replace this object with your actual Firebase config!
 const firebaseConfig = {
   apiKey: "AIzaSyCGxY-GTffKT1d5iN1DSMOzPA5P73NCeRw",
   authDomain: "student-dashboard-514ce.firebaseapp.com",
   projectId: "student-dashboard-514ce",
-  storageBucket: "student-dashboard-514ce.firebasestorage.app",
+  storageBucket: "student-dashboard-514ce.appspot.com",
   messagingSenderId: "1037843166676",
   appId: "1:1037843166676:web:1ed5d4f90835ad5f919241",
   measurementId: "G-28NXMQNQPS"
@@ -18,6 +19,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, db, provider };
+export { app, auth, db, storage, provider };
